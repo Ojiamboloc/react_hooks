7094b24b82168610a5eb91f34998688dc9cdda30
Ojiamboloc <ojiamboloc@gmail.com>
2022-12-31 01:15:07 -0800

    added useimperativetoggle

---
+++ src/components/Uimperativehandle.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "./Button";
+
+const Uimperativehandle = () => {
+  
+  return (
+    <div>
+     <button >Button From Parent</button>
+     <Button />
+    </div>
+  );
+};
+
+export default Uimperativehandle;
