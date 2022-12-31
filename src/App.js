dd4fe7ab62367bedc580c9e952106813bee9037a
Ojiamboloc <ojiamboloc@gmail.com>
2022-12-31 03:00:01 -0800

    added useMemo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 //import Ustate from './components/Ustate'
 import './App.css';
+import Umemo from './components/Umemo';
 //import Uimperativehandle from './components/Uimperativehandle';
 //import Ulayouteffect from './components/Ulayouteffect';
 //import Ueffect from './components/Ueffect';
 //import Ureducer from './components/Ureducer';
 //import Uref from './components/Uref'
-import Ucontext from './components/Ucontext';
+//import Ucontext from './components/Ucontext';
 function App() {
   return (
     <div className="App">
-    <Ucontext />
+    <Umemo />
     </div>
   );
 }
